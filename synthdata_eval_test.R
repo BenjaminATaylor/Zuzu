1a57bf5ca6673b19c6e84533605b177ce949190c
Benjamin Taylor <benjamin.aaron.taylor@gmail.com>
2024-10-10 17:34:21 +0100

    Add genewise variance plot to synthtest

--- synthdata_eval_test.R
+++ synthdata_eval_test.R
@@ -54,7 +54,7 @@ dispersions.alt.true[is.na(dispersions.alt.true)] = 0
 
 
 #### Synthetic data generation ####
-# Generate a matching synthetic dataset using compcodeR
+## Generate a matching synthetic dataset using compcodeR
 # Get means of the reference level in the real data
 refmeans.true = rowMeans(counts.true[,metadata.true$phenotype == "nurse"])
 dispersions.true = data.frame(ref = dispersions.ref.true, alt = dispersions.alt.true) %>% as.matrix()
@@ -90,6 +90,40 @@ dds.compcoder = DESeqDataSetFromMatrix(countData = counts.compcoder,
 dds.compcoder = DESeq(dds.compcoder)
 res.compcoder = results(dds.compcoder)
 
+## Generate another synthetic dataset using seqgendiff
+# Here we apply a signal to 10% of genes (1-prop_null), and with a log effect distributed in a normal distribution with mean 0 and standard deviation 0.8
+prop.deg = nDEGs/nrow(counts.true)
+counts.ref.true = counts.true[,metadata.true$phenotype == "nurse"]
+
+# The distribution of effect sizes could be tweaked to fit more closely to the true data, perhaps by trying to fit a distribution to the effect sizes of DEGs in the true data
+thout = thin_2group(mat = as.matrix(counts.ref.true),
+                    prop_null = 1-prop.deg, 
+                    signal_fun = stats::rnorm,
+                    signal_params = list(mean = 0, sd = 0.8))
+thout$mat
+
+# Hmm but here we have 2 issues. 1) We only have the sample size from the control data, not the original data;
+# and 2) The effects have been distributed to a random subset of samples, instead of us choosing which groups are true and false
+# 2) is fine (can rearrange posthoc) but how to deal with 1)?
+# For now we'll just have to live with it
+#Extract counts
+counts.seqgendiff = thout$mat %>% 
+  data.frame() %>%
+  `colnames<-`(paste0("sample_",1:ncol(.))) %>%
+  `rownames<-`(row.names(counts.true.nurse))
+# Extract metadata
+tmp = thout$designmat %>% 
+  data.frame() %>% 
+  mutate(phenotype = ifelse(P1 == 0, "nurse", "forager")) %>%
+  mutate(sample = paste0("sample_",1:nrow(.))) %>%
+  select(sample, phenotype)
+metadata.seqgendiff = tmp[order(tmp$phenotype),]
+counts.seqgendiff = counts.seqgendiff[,metadata.seqgendiff$sample]
+all(colnames(counts.seqgendiff) == metadata.seqgendiff$sample)
+
+
+
+
 #### Testing dataset conformity ####
 #Now we can test how well this new dataset resembles the true dataset
 
@@ -101,7 +135,8 @@ ggplot(mapping = aes(x = quantile(log(qqdata.compcoder$true+1), quantiles, type 
   geom_point() +
   geom_abline(aes(slope = 1, intercept = 0), linetype = 2)  +
   geom_smooth(method = "lm") +
-  coord_equal()
+  coord_equal() +
+  theme_bw()
 
 # Get goodness of fit metrics for the quantiles
 ccc.compcoder.log = CCC(log(qqdata.compcoder$true+1), 
@@ -113,6 +148,11 @@ ccc.compcoder.raw = CCC(qqdata.compcoder$true,
 # Much higher for the log-transformed values
 ccc.compcoder.log$rho.c
 ccc.compcoder.raw$rho.c
+
+# Okay now we can plot these data together?
+#pseudocode: we can either plot the qqs together,or make single plot with the qq fits for each method plotted next to each other (ideally we would have many fits from many repeated simulations, so then we could plot as a box or something like that)
+
+
 
 # (Note that for these sample sizes, K-S tests are too sensitive to be of much use)
 
@@ -145,14 +185,14 @@ ggplot(meanvar.compcoder, aes(x = mean, y = var)) +
 #### Next check if feature-feature correlations are preserved ####
 # Take a subsample of genes, since doing this for all pairwise comparisons would be too computationally intense
 varied.counts.true = counts.true[which(rowVars(log2(counts.true+1))!=0),] 
-varied.counts.subset.true = varied.counts.true[sample(nrow(varied.counts.true), 25),]
+varied.counts.subset.true = varied.counts.true[sample(nrow(varied.counts.true), 500),]
 # Get pairwise spearman cor between each pair of genes
 corrs.mat.true = cor(t(varied.counts.subset.true), method = "spearman")
 corrs.true = corrs.mat.true[lower.tri(corrs.mat.true)]
 
 # Repeat for compcoder
 varied.counts.compcoder = counts.compcoder[which(rowVars(log2(counts.compcoder+1))!=0),] 
-varied.counts.subset.compcoder = varied.counts.compcoder[sample(nrow(varied.counts.compcoder), 25),]
+varied.counts.subset.compcoder = varied.counts.compcoder[sample(nrow(varied.counts.compcoder), 500),]
 # Get pairwise spearman cor between each pair of genes
 corrs.mat.compcoder = cor(t(varied.counts.subset.compcoder), method = "spearman")
 corrs.compcoder = corrs.mat.compcoder[lower.tri(corrs.mat.compcoder)]
@@ -163,9 +203,12 @@ ggplot(corrs.ggdata, aes(x = variable, y = value)) +
   geom_violin() +
   theme_bw()
 
-# A simple t-test will do to compare these two distributions statistically
+# A simple t-test and also K-S test
 t.test(corrs.ggdata$value ~ corrs.ggdata$variable)
-
+ks.test(corrs.true, corrs.compcoder)
+# A better was of testing this might be the Wasserstein distance?
+library("transport")
+wasserstein1d(a = corrs.true, b = corrs.compcoder)
 
 #### Volcano plots ####
 # We can also compare the volcano plots of the two datasets
@@ -220,17 +263,45 @@ counts_pca(counts.true, metadata.true)
 counts_pca(counts.compcoder, metadata.compcoder)
 
 #### Check per-gene expression level distributions ####
-ggplot(data.frame(true = rowMeans(log(counts.true+1)), compcoder = rowMeans(log(counts.compcoder+1))) %>% 
+logs.true = rowMeans(log(counts.true+1))
+logs.compcoder = rowMeans(log(counts.compcoder+1))
+
+ggplot(data.frame(true = logs.true, compcoder = logs.compcoder) %>% 
          reshape2::melt(), aes(x = value, fill = variable)) + 
   geom_density(alpha = 0.3) +
   labs(x = "Per-gene log mean expression level", y = "Density", fill = "Method") +
   theme_bw()
 
+t.test(logs.true, logs.compcoder)
+ks.test(logs.true, logs.compcoder)
+wasserstein1d(a = logs.true, b = logs.compcoder)
+
+
 #### Distribution of 0s per gene
-ggplot(data.frame(true = rowSums(counts.true == 0), compcoder = rowSums(counts.compcoder == 0)) %>% 
+zeros.true = rowSums(counts.true == 0)
+zeros.compcoder = rowSums(counts.compcoder == 0)
+
+ggplot(data.frame(true = zeros.true, compcoder = zeros.compcoder) %>% 
          reshape2::melt(), aes(x = value, fill = variable)) + 
   geom_density(alpha = 0.3) +
   labs(x = "Per-gene 0 count frequency", y = "Density", fill = "Method") +
   theme_bw()
 
+t.test(zeros.true, zeros.compcoder)
+ks.test(zeros.true, zeros.compcoder)
+wasserstein1d(a = zeros.true, b = zeros.compcoder)
 
+
+#### Distribution of per-gene variances
+vars.true = log(meanvar.true$var+1)
+vars.compcoder = log(meanvar.compcoder$var+1)
+
+ggplot(data.frame(true = vars.true, compcoder = vars.compcoder) %>% 
+         reshape2::melt(), aes(x = value, fill = variable)) + 
+  geom_density(alpha = 0.3) +
+  labs(x = "Per-gene log variance", y = "Density", fill = "Method") +
+  theme_bw()
+
+t.test(vars.true, vars.compcoder)
+ks.test(vars.true, vars.compcoder)
+wasserstein1d(a = vars.true, b = vars.compcoder)
